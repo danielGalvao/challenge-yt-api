d6927ca3b8dfa8fdf55d043a4feabf27e5d3f221
Daniel Galvão <galvaodev@gmail.com>
2017-04-01 00:41:10 -0300

    Improving css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,6 +6,12 @@ export default class Header extends Component {
 	render() {
 		return (
 			<header className="header">
+				<div className="header__content">
+					<div className="header__brand">
+						<img src="" alt="Fictícia Vídeos" title="Fictícia Vídeos" className="header__logo" />
+					</div>
+					<a className="header__menu fa fa-bars fa-2" title="Menu"></a>
+				</div>
 			</header>
 		)
 	}
--- src/main/app.jsx
+++ src/main/app.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 
 import Header from '../components/Header'
+import resetCss from '../styles/reset.scss'
 
 export default props => (
   <main>
     <Header />
-    <h1>App</h1>
+    <div>
+      <h1>App</h1>
+    </div>
   </main>
 )
