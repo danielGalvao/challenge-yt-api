3d6ab003be33243e1f8aa10035d11fb69b4d50e2
Daniel Galvão <galvaodev@gmail.com>
2017-04-01 23:52:59 -0300

    Improving header, logo, fonts and resets

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import React, { Component, PropTypes } from 'react'
 // Local imports
 import headerStyle from '../styles/header.scss'
+import logoHeader from '../assets/images/logo.png'
 
 export default class Header extends Component {
 	render() {
@@ -8,9 +9,9 @@ export default class Header extends Component {
 			<header className="header">
 				<div className="header__content">
 					<div className="header__brand">
-						<img src="" alt="Fictícia Vídeos" title="Fictícia Vídeos" className="header__logo" />
+						<img src={logoHeader} alt="Fictícia Vídeos" title="Fictícia Vídeos" className="header__logo" />
 					</div>
-					<a className="header__menu fa fa-bars fa-2" title="Menu"></a>
+					<a className="header__menu" title="Menu">Menu <i className="header__menu-icon fa fa-bars fa-2"></i></a>
 				</div>
 			</header>
 		)
