b114eef4f8cb4b77733e29412845b6c604303d57
Daniel Galvão <galvaodev@gmail.com>
2017-04-03 00:49:11 -0300

    Improving css, start youtube api and video details components

---
+++ src/components/youtube/YoutubeApi.jsx
@@ -0,0 +1,28 @@
+import axios from 'axios'
+import * as YT from './YoutubeVars'
+
+module.exports = function (opt, callback) {
+
+  var params = {
+    part: 'snippet',
+    key: YT.API_KEY,
+    channelId: YT.CHANNEL_ID,
+    q: opt.term,
+    maxResults: YT.MAX_RESULTS,
+    type: 'video'
+  }
+
+  axios.get(YT.API_URL, { params: params })
+    .then(function(resp) {
+      if (callback) {
+        callback(resp.data.items)
+      }
+    })
+    .catch(function(error) {
+      console.error(error)
+    });
+};
+
+/*
+YoutuveApi({term: term}, (videos) => {});
+*/
---
+++ src/components/youtube/YoutubeVars.js
@@ -0,0 +1,5 @@
+export const API_KEY = ''
+export const MIN_INCREMENTS = 5
+export const MAX_RESULTS = 20
+export const CHANNEL_ID = ''
+export const API_URL = 'https://www.googleapis.com/youtube/v3/search'
