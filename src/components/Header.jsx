9b25f777dab46b682722cb08fa563f5d84b53533
Daniel Galvão <daniel.galvao@justdigital.com.br>
2017-04-03 22:02:23 -0300

    Update css add search and fix containers

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,7 +11,8 @@ export default class Header extends Component {
 					<div className="header__brand">
 						<img src={logoHeader} alt="Fictícia Vídeos" title="Fictícia Vídeos" className="header__logo" />
 					</div>
-					<a className="header__menu" title="Menu">Menu <i className="header__menu-icon fa fa-bars fa-2"></i></a>
+					<a className="header__search" title="Pesquisar"><i className="header__search-icon fa fa-search"></i></a>
+					<a className="header__menu" title="Menu">Menu <i className="header__menu-icon fa fa-bars"></i></a>
 				</div>
 			</header>
 		)
