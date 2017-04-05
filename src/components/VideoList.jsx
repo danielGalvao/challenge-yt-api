85327fe1dbcdd3ba760a7352d26368f215606fa6
Daniel Galvão <galvaodev@gmail.com>
2017-04-05 00:40:22 -0300

    Create video css, improving ux and css of components

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 // Local imports
 import listCss from '../styles/videoList.scss'
+import videoCss from '../styles/video.scss'
 
 export default class VideoHighlight extends Component {
 	render() {
@@ -11,12 +12,36 @@ export default class VideoHighlight extends Component {
 				</div>
         <div className="list__container">
           <ul>
-						<li>Video1</li>
-						<li>Video2</li>
-						<li>Video3</li>
-						<li>Video4</li>
+						<li className="video">
+							<img className="video__image" />
+							<p className="video__title">Conheça o MAPA de carreiras do VAGAS.com testando reticencias</p>
+							<span className="video__views">
+								<i className="video__views-icon fa fa-eye"></i>10 views
+							</span>
+						</li>
+						<li className="video">
+							<img className="video__image" />
+							<p className="video__title">Conheça o MAPA de carreiras do VAGAS.com testando reticencias</p>
+							<span className="video__views">
+								<i className="video__views-icon fa fa-eye"></i>5k views
+							</span>
+						</li>
+						<li className="video">
+							<img className="video__image" />
+							<p className="video__title">Conheça o MAPA de carreiras do VAGAS.com testando reticencias</p>
+							<span className="video__views">
+								<i className="video__views-icon fa fa-eye"></i>1k views
+							</span>
+						</li>
+						<li className="video">
+							<img className="video__image" />
+							<p className="video__title">Conheça o MAPA de carreiras do VAGAS.com</p>
+							<span className="video__views">
+								<i className="video__views-icon fa fa-eye"></i>100 views
+							</span>
+						</li>
           </ul>
-					<span className="list__more">carregar mais vídeos</span>
+					<span className="list__more">carregar mais vídeos...</span>
         </div>
 			</div>
 		)
