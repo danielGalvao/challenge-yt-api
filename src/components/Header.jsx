e8b090ebab16f5894de60aea964c04ac7fb81301
Daniel Galvão <galvaodev@gmail.com>
2017-04-05 23:58:30 -0300

    Add menu and ux menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 // Local imports
 import headerStyle from '../styles/header.scss'
+import menuStyle from '../styles/menu.scss'
 import logoHeader from '../assets/images/logo.png'
 
 export default class Header extends Component {
@@ -12,7 +13,19 @@ export default class Header extends Component {
 						<img src={logoHeader} alt="Fictícia Vídeos" title="Fictícia Vídeos" className="header__logo" />
 					</div>
 					<a className="header__search" title="Pesquisar"><i className="header__search-icon fa fa-search"></i></a>
-					<a className="header__menu" title="Menu">Menu <i className="header__menu-icon fa fa-bars"></i></a>
+					<div className="header__menu" title="Menu">Menu
+						<i className="header__menu-icon fa fa-bars"></i>
+					</div>
+					<ul className="menu">
+						<li className="menu__item">
+							<i className="menu__item-icon fa fa-star-o"></i>
+							Destaques
+						</li>
+						<li className="menu__item">
+							<i className="menu__item-icon fa fa-toggle-right"></i>
+							Vídeos
+						</li>
+					</ul>
 				</div>
 			</header>
 		)
