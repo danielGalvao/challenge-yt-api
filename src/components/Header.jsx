16e6e21e547e7c7ce7f9508e1453de5687de31e4
Daniel Galvão <galvaodev@gmail.com>
2017-04-07 10:44:47 -0300

    Add ux with transition and close search

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,16 +5,32 @@ import menuStyle from '../styles/menu.scss'
 import logoHeader from '../assets/images/logo.png'
 
 export default class Header extends Component {
+
+	constructor(props){
+    super(props);
+		this.handleSearch = this.handleSearch.bind(this);
+		this.state = {
+    	activeSearch: false,
+    };
+	}
+
+  handleSearch(){
+			const currentState = this.state.activeSearch;
+      this.setState( { activeSearch : !currentState } );
+  }
+
 	render() {
+		let classNameSearch = this.state.activeSearch ? 'header__search active' : 'header__search'
+		let classNameSearchIcon = this.state.activeSearch ? "header__search-icon fa fa-close" : "header__search-icon fa fa-search"
 		return (
 			<header className="header">
 				<div className="header__content">
 					<div className="header__brand">
 						<img src={logoHeader} alt="Fictícia Vídeos" title="Fictícia Vídeos" className="header__logo" />
 					</div>
-					<div className="header__search" title="Pesquisar">
+					<div className={classNameSearch} title="Pesquisar">
 						<input type="text" className="header__search-input" />
-						<i className="header__search-icon fa fa-search"></i>
+						<i className={classNameSearchIcon} onClick={this.handleSearch}></i>
 					</div>
 					<div className="header__menu" title="Menu">Menu
 						<i className="header__menu-icon fa fa-bars"></i>
