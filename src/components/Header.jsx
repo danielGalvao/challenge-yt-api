f744b1fd92c53605799520feedcc0191c3eb21fa
Daniel Galvão <galvaodev@gmail.com>
2017-04-07 00:44:58 -0300

    Improving css and add search am ux

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,7 +12,10 @@ export default class Header extends Component {
 					<div className="header__brand">
 						<img src={logoHeader} alt="Fictícia Vídeos" title="Fictícia Vídeos" className="header__logo" />
 					</div>
-					<a className="header__search" title="Pesquisar"><i className="header__search-icon fa fa-search"></i></a>
+					<div className="header__search" title="Pesquisar">
+						<input type="text" className="header__search-input" />
+						<i className="header__search-icon fa fa-search"></i>
+					</div>
 					<div className="header__menu" title="Menu">Menu
 						<i className="header__menu-icon fa fa-bars"></i>
 					</div>
