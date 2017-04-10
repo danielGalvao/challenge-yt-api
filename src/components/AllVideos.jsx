e93c4b145b10b57848a5309fe2876b5141c512df
Daniel Galvão <galvaodev@gmail.com>
2017-04-09 23:41:06 -0300

    Starting all videos

--- src/components/AllVideos.jsx
+++ src/components/AllVideos.jsx
@@ -4,6 +4,7 @@ import YoutubeApi from '../components/youtube/YoutubeApi'
 // Local imports
 import VideoItem from './video/VideoItem'
 import listCss from '../styles/videoList.scss'
+import allVideos from '../styles/allvideos.scss'
 
 export default class AllVideos extends Component {
 	constructor(props) {
@@ -12,17 +13,24 @@ export default class AllVideos extends Component {
     this.state = {
       term: '',
       videos: [],
+			totalVideos: 20
     };
 
-    this.videoSearch(this.state.term)
+    this.videoSearch(this.state.term, this.state.totalVideos)
   }
 
-  videoSearch(term) {
-    YoutubeApi({term: term, maxResults: 20}, (videos) => {
+  videoSearch(term, totalVideos) {
+    YoutubeApi({term: term, maxResults: totalVideos}, (videos) => {
       this.setState({
         videos: videos
       })
     })
+  }
+
+	loadVideos(moreVideos) {
+    var newTotal = Number(this.state.totalVideos) + Number(moreVideos);
+    this.setState({totalVideos: newTotal});
+    this.videoSearch(this.state.term, newTotal);
   }
 
 	render() {
@@ -36,11 +44,16 @@ export default class AllVideos extends Component {
 		})
 
 		return (
-			<div className="container">
-				<h1>Todos os vídeos</h1>
-				<ul>
-					{videoItems}
-				</ul>
+			<div className="container_allvideos">
+				<div className="container_allvideos__title">
+					<h1>Todos os vídeos do Canal</h1>
+				</div>
+				<div className="allvideos">
+					<ul>
+						{videoItems}
+					</ul>
+					<span onClick={event => this.loadVideos(this.state.totalVideos)} className="allvideos__more">carregar mais vídeos...</span>
+				</div>
 			</div>
 		)
 	}
